3c5e3eb80f31dda20cfcbbe954d32ac6fbebf13f
Eason <eason991@gmail.com>
2016-02-29 00:04:22 +0800

    op img output

--- img2html.php
+++ img2html.php
@@ -5,33 +5,48 @@
  * Date: 16/2/26
  * Time: 下午6:39
  */
-
 // filename
 $imgFile = 'img.jpg';
 // pixel step
-$step = 5;
+// 
 // alpha channel
-$alpha = 0.5;
+$alpha = 1;
+// rotate angle
+$angle = 90;
 
 list($width, $height, $type, $attr) = getimagesize($imgFile);
 $im = imagecreatefromjpeg($imgFile);
+$im = imagerotate($im, $angle, 0);
+
+// rotate fix
+$t = $width;
+$width = $height;
+$height = $t;
+
+$ratio = max($width, $height)*0.15;
+$widthStep = $width/$ratio;
+$heightStep = $height/$ratio;
 
 ob_start();
+echo '
+<style type="text/css">
+.block{
+	width:1px;
+	height:1px;
+	font-size:1px;
+}
+</style>';
 
-for ($i = 0; $i <= $width; ($i += $step)) {
+for ($i = 0; $i < $width; ($i += $widthStep)) {
+    for ($j = 0; $j < $height; ($j += $heightStep)) {
 
-    for ($j = 0; $j <= $height; ($j += $step)) {
-
-        $rgb = ImageColorAt($im, $j, $i);
+        $rgb = ImageColorAt($im, $i, $j);
         $r = ($rgb >> 16) & 0xFF;
         $g = ($rgb >> 8) & 0xFF;
         $b = $rgb & 0xFF;
-
-        printf('<span style="color:rgba(%u, %u, %u, %.1f);">▇</span>', $r, $g, $b, $alpha);
+        printf('<span class="block" style="color:rgba(%u, %u, %u, %.1f);">▇</span>', $r, $g, $b, $alpha);
     }
-
     echo "<br/>";
 }
 
 ob_flush();
-
